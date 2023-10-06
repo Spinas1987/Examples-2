ed68bb2ee5b95e1dce1274cd09fbbb44a5b8b62f
Morteza <spinas.ir@yandex.ru>
2023-10-06 19:07:10 +0300

    Задача 42

---
+++ Задача 42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int DecToBaseNum(int number, int baseNum)
+{
+    int result = 0;
+    int mult = 1;
+    while (number > 0)
+    {
+        result += number % baseNum * mult;
+        number /= baseNum;
+        mult *= 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите основане системы счисления");
+int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+int res = DecToBaseNum(number, baseNumber);
+Console.WriteLine($"{number} - > {res}");
